b4e587aefa5c35b687b14ecdb91d270abe97282c
Ben <bpheasley@gmail.com>
2024-06-17 15:36:52 -0500

    Adding basic price display

--- main.py
+++ main.py
@@ -20,25 +20,42 @@
 # crypto_historical_client = CryptoHistoricalDataClient(public_key, private_key)
 
 import sqlite3, config
-from fastapi import FastAPI
-
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 
 @app.get("/")
-def index():
+def index(request: Request):
     connection = sqlite3.connect(config.DATABASE)
     connection.row_factory = sqlite3.Row
     cursor = connection.cursor()
 
     cursor.execute("""
-        SELECT symbol, name FROM stock
+        SELECT id, symbol, name FROM stock ORDER BY symbol
     """)
 
     rows = cursor.fetchall()
 
-    return {"message": "Dashboard", "stocks": rows}
+    return templates.TemplateResponse("index.html", {"request": request, "stocks": rows})
 
+@app.get("/stock/{symbol}")
+def stock_detail(request: Request, symbol: str):
+    connection = sqlite3.connect(config.DATABASE)
+    connection.row_factory = sqlite3.Row
+    cursor = connection.cursor()
+
+    cursor.execute("""
+        SELECT id, symbol, name FROM stock where symbol = ?
+    """, (symbol,))
+    row = cursor.fetchone()
+
+    cursor.execute("""
+        SELECT * from stock_price where stock_id = ?
+        """, (row['id'],))
+    prices = cursor.fetchall()
+    return templates.TemplateResponse("stock_detail.html", {"request": request, "stock": row, "bars": prices})
 
 # # create historical data request object
 # request_params = StockBarsRequest(
