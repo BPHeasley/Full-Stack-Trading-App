066449fa3f4d9e52979a8ce956d8c8e4dff028aa
Ben <bpheasley@gmail.com>
2024-06-18 16:46:47 -0500

    Created strategy list and ability to add stocks to it

--- main.py
+++ main.py
@@ -20,7 +20,8 @@ from datetime import datetime
 # crypto_historical_client = CryptoHistoricalDataClient(public_key, private_key)
 
 import sqlite3, config
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Form
+from fastapi.responses import RedirectResponse
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
@@ -101,6 +102,43 @@ def stock_detail(request: Request, symbol: str):
     strategies = cursor.fetchall()
     return templates.TemplateResponse("stock_detail.html", {"request": request, "stock": row, "bars": prices,
                                                             "strategies": strategies})
+
+@app.post("/apply_strategy")
+def apply_strategy(strategy_id: int = Form(), stock_id: int = Form()):
+    connection = sqlite3.connect(config.DATABASE)
+    cursor = connection.cursor()
+
+    cursor.execute("""
+    INSERT INTO stock_strategy (stock_id, strategy_id) VALUES (?, ?)
+    """, (stock_id, strategy_id))
+
+    connection.commit()
+
+    return RedirectResponse(url=f"/strategy/{strategy_id}", status_code=303)
+
+@app.get("/strategy/{strategy_id}")
+def strategy_detail(request: Request, strategy_id: int):
+    connection = sqlite3.connect(config.DATABASE)
+    connection.row_factory = sqlite3.Row
+    cursor = connection.cursor()
+
+    cursor.execute("""
+    SELECT id, name
+    FROM strategy
+    where id = ?
+    """, (strategy_id,))
+
+    strategy = cursor.fetchone()
+
+    cursor.execute("""
+    SELECT symbol, name
+    FROM stock JOIN stock_strategy ON stock.id = stock_strategy.stock_id
+    where strategy_id = ?
+    """, (strategy_id,))
+
+    stocks = cursor.fetchall()
+
+    return templates.TemplateResponse("strategy.html", {"request": request, "stocks": stocks, "strategy": strategy})
 
 # # create historical data request object
 # request_params = StockBarsRequest(
